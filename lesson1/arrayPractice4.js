d6348ff0c1fdf11ad2bfe66b9e39756571538392
Sergio Ravera <sergioravera@hotmail.de>
2023-08-03 13:54:03 +0200

    added array practice 4

---
+++ lesson1/arrayPractice4.js
@@ -0,0 +1,11 @@
+// What is the return value of the following statement? Why?
+['ant', 'bear', 'caterpillar'].pop().length;
+
+//Answer:
+/*
+1. the pop method is called on the array ['ant', 'bear', 'caterpillar']
+2. the pop method removes the last element within the array "caterpillar"
+3. the return value of pop is the removed element "caterpillar"
+4. the length property is used on said string counting it's letters.
+5. return value = 11;
+*/
