91f3461d43e93e284fb4582f36d452c59a0b0eae
Sergio Ravera <sergioravera@hotmail.de>
2023-08-15 12:43:47 +0200

    added Palendromic Numbers

---
+++ SmallProblems/easy4/Nr4_PalindromicNumbers.js
@@ -0,0 +1,9 @@
+// Write a function that returns true if its integer argument is palindromic,
+// or false otherwise. A palindromic number reads the same forwards and backwards.
+
+const isPalindromicNumber = (num) => num === Number(String(num).split("").reverse().join(""));
+
+console.log(isPalindromicNumber(34543));        // true
+console.log(isPalindromicNumber(123210));       // false
+console.log(isPalindromicNumber(22));           // true
+console.log(isPalindromicNumber(5));            // true
