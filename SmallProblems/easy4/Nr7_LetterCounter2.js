b64603f6a1e89a238a21ce0ec229bab2a9456310
Sergio Ravera <sergioravera@hotmail.de>
2023-08-16 16:38:55 +0200

    added Letter Counter 2

---
+++ SmallProblems/easy4/Nr7_LetterCounter2.js
@@ -0,0 +1,42 @@
+// Write a function that takes a string consisting of zero or more space separated words
+// and returns an object that shows the number of words of different sizes.
+
+// Modify the wordSizes function from the previous exercise to exclude non-letters 
+// when determining word size. For instance, the word size of "it's" is 3, not 4.
+
+function wordSizes(words) {
+  let wordsArray = words.split(' ');
+  let count = {};
+
+  for (let idx = 0; idx < wordsArray.length; idx += 1) {
+    let cleanWordSize = removeNonLetters(wordsArray[idx].toLowerCase()).length;
+    if (cleanWordSize === 0) continue;
+ 
+    count[cleanWordSize] = count[cleanWordSize] || 0;
+    count[cleanWordSize] += 1;
+  }
+
+  return count;
+}
+
+function removeNonLetters(string) {
+  let result = '';
+
+  for (let idx = 0; idx < string.length; idx += 1) {
+    if (isLetter(string[idx])) {
+      result += string[idx];
+    }
+  }
+
+  return result;
+}
+
+function isLetter(char) {
+  return char >= 'a' && char <= 'z';
+}
+
+
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
+console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes(''));                                            // {}
