df5eca3fdd37ed1d4856a447c53cd1e0c2f8c376
Sergio Ravera <sergioravera@hotmail.de>
2023-08-17 18:42:47 +0200

    added more structural functions

--- lesson3/Twenty-One.js
+++ lesson3/Twenty-One.js
@@ -28,10 +28,26 @@ const ACE = "A";
 const FACE_10 = ["J", "Q", "K"];
 const HIT_OR_STAY = [["hit", "h"], ["stay", "s"]];
 
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+function answerIsHitOrStay(answer) {
+  return HIT_OR_STAY[0].includes(answer) || HIT_OR_STAY[1].includes(answer)
+}
+
 function repeatUserInput(msg) {
-  console.log(msg);
+  prompt(msg);
 
   return RL_SYNC.question();
+}
+
+function shuffle(deck) {
+  for (let index = deck.length - 1; index > 0; index--) {
+    let otherIndex = Math.floor(Math.random() * (index + 1));  // num between 0 and index
+    [deck[index], deck[otherIndex]] = [deck[otherIndex], deck[index]];  //swap cards
+  }
+  return deck;
 }
 
 function initializeDeck() {
@@ -50,21 +66,10 @@ function initializeDeck() {
     if (card[1] === "14") card[1] = "A";
   });
 
-  return deck;
+  return shuffle(deck);
 }
 
-function answerIsHitOrStay(answer) {
-  return HIT_OR_STAY[0].includes(answer) || HIT_OR_STAY[1].includes(answer)
-}
-
-function shuffleDeck(deck) {
-  for (let index = deck.length - 1; index > 0; index--) {
-    let otherIndex = Math.floor(Math.random() * (index + 1));  // num between 0 and index
-    [deck[index], deck[otherIndex]] = [deck[otherIndex], deck[index]];  //swap cards
-  }
-}
-
-function total(cards) {
+function totalCardsValue(cards) {
   let values = cards.map(card => card[1]);
   let sum = 0;
 
@@ -85,34 +90,104 @@ function total(cards) {
   return sum;
 }
 
-function playerTurn() {
-  do {
-    console.log(("hit or stay? (h/s)"));
-    let answer = RL_SYNC.question();
+function busted(cards) {
+  return totalCardsValue(cards) > 21;
+}
 
-    while (!answerIsHitOrStay(answer)) {
-      answer = repeatUserInput("no...hit or stay? Those are the only options");
+function playerTurn(playerTurn) {
+  do {
+    prompt(("hit or stay? (h/s)"));
+    playerTurn = RL_SYNC.question().toLowerCase();
+
+    while (!answerIsHitOrStay(playerTurn)) {
+      playerTurn = repeatUserInput("no...hit or stay? Those are the only options");
     }
 
-  } while (answer !== "stay" || busted());
+    if (HIT_OR_STAY[0].includes(playerTurn)) {
+      // insert action of drawing a card
+    }
+
+  } while (!HIT_OR_STAY[1].includes(playerTurn) || !busted());
 
   if (busted()) {
     // end game. Play again?
   } else {
-    console.log("You chose to stay!");
+    prompt("You chose to stay!");
   }
 }
 // work in process
 function dealerTurn(cardsValue) {
+  let answer;
   do {
-    console.log(("hit or stay? (h/s)"));
-    let answer = RL_SYNC.question();
+    if (cardsValue >= 17) {
+      prompt("hit or stay? (h/s)");
+      answer = RL_SYNC.question();
+    } else {
+      answer = "hit";
+      prompt(`Dealer Value is too low to stay`);
+    }
   } while (answer !== "stay" || busted());
 
   if (busted()) {
     // end game. Play again?
   } else {
-    console.log("You chose to stay!");
+    prompt("Dealer chose to stay!");
   }
 }
 
+function getResult(playerCards, dealerCards) {
+  let playerTotal = totalCardsValue(playerCards);
+  let dealerTotal = totalCardsValue(dealerCards);
+
+  if (playerTotal > 21) {
+    return 'PLAYER_BUSTED';
+  } else if (dealerTotal > 21) {
+    return 'DEALER_BUSTED';
+  } else if (dealerTotal < playerTotal) {
+    return 'PLAYER';
+  } else if (dealerTotal > playerTotal) {
+    return 'DEALER';
+  } else {
+    return 'TIE';
+  }
+}
+
+function displayResult(playerCards, dealerCards) {
+  let result = getResult(playerCards, dealerCards);
+
+  switch (result) {
+    case 'PLAYER_BUSTED':
+      prompt('You busted! Dealer wins!');
+      break;
+    case 'DEALER_BUSTED':
+      prompt('Dealer busted! You win!');
+      break;
+    case 'PLAYER':
+      prompt('You win!');
+      break;
+    case 'DEALER':
+      prompt('Dealer wins!');
+      break;
+    case 'TIE':
+      prompt("It's a tie!");
+  }
+}
+
+function playRound() {
+  while (true) {
+    let deck = initializeDeck();
+    let playerCards = [];
+    let dealerCards = [];
+
+    getInitialHands(playerCards, dealerCards, deck);
+    displayInitialHands(playerCards, dealerCards);
+
+    playerTurn(playerCards, deck);
+    dealerTurn(dealerCards, deck);
+
+    getResult(playerCards, dealerCards);
+    displayResult(playerCards, dealerCards);
+
+    if (!playAgain()) break;
+  }
+}
