ed847846832590559d7bf34346ba57a0417f35fb
Sergio Ravera <sergioravera@hotmail.de>
2023-08-17 15:58:59 +0200

    added foundational functions

--- lesson3/Twenty-One.js
+++ lesson3/Twenty-One.js
@@ -15,9 +15,24 @@ ALGORITHM:
 6. If dealer busts, player wins.
 7. Compare cards and declare winner.
 
-*/
 
+3. Player turn:
+- Ask player to hit or stay.
+- If stay, stop asking.
+- if hit && sum > 21 => bust
+- Otherwise, go to 1.
+*/
+const RL_SYNC = require("readline-sync");
 const DECK_OF_CARDS = initializeDeck();
+const ACE = "A";
+const FACE_10 = ["J", "Q", "K"];
+const HIT_OR_STAY = [["hit", "h"], ["stay", "s"]];
+
+function repeatUserInput(msg) {
+  console.log(msg);
+
+  return RL_SYNC.question();
+}
 
 function initializeDeck() {
   let deck = [];
@@ -38,4 +53,66 @@ function initializeDeck() {
   return deck;
 }
 
-console.log(DECK_OF_CARDS.length);
+function answerIsHitOrStay(answer) {
+  return HIT_OR_STAY[0].includes(answer) || HIT_OR_STAY[1].includes(answer)
+}
+
+function shuffleDeck(deck) {
+  for (let index = deck.length - 1; index > 0; index--) {
+    let otherIndex = Math.floor(Math.random() * (index + 1));  // num between 0 and index
+    [deck[index], deck[otherIndex]] = [deck[otherIndex], deck[index]];  //swap cards
+  }
+}
+
+function total(cards) {
+  let values = cards.map(card => card[1]);
+  let sum = 0;
+
+  values.forEach(value => {
+    if (value === "A") {
+      sum += 11;
+    } else if (FACE_10.includes(value)) {
+      sum += 10;
+    } else {
+      sum += Number(value);
+    }
+  });
+
+  values.filter(value => value === ACE).forEach(_ => {
+    if (sum > 21) sum -= 10;
+  });
+
+  return sum;
+}
+
+function playerTurn() {
+  do {
+    console.log(("hit or stay? (h/s)"));
+    let answer = RL_SYNC.question();
+
+    while (!answerIsHitOrStay(answer)) {
+      answer = repeatUserInput("no...hit or stay? Those are the only options");
+    }
+
+  } while (answer !== "stay" || busted());
+
+  if (busted()) {
+    // end game. Play again?
+  } else {
+    console.log("You chose to stay!");
+  }
+}
+// work in process
+function dealerTurn(cardsValue) {
+  do {
+    console.log(("hit or stay? (h/s)"));
+    let answer = RL_SYNC.question();
+  } while (answer !== "stay" || busted());
+
+  if (busted()) {
+    // end game. Play again?
+  } else {
+    console.log("You chose to stay!");
+  }
+}
+
