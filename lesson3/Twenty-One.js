8d6e7ea37fc1ece17367303dd84c36205eaa3e94
Sergio Ravera <sergioravera@hotmail.de>
2023-08-18 16:09:16 +0200

    finished Twenty One draft

--- lesson3/Twenty-One.js
+++ lesson3/Twenty-One.js
@@ -23,7 +23,7 @@ ALGORITHM:
 - Otherwise, go to 1.
 */
 const RL_SYNC = require("readline-sync");
-const DECK_OF_CARDS = initializeDeck();
+const YES_OR_NO = ["yes", "y", "no", "n"];
 const ACE = "A";
 const FACE_10 = ["J", "Q", "K"];
 const HIT_OR_STAY = [["hit", "h"], ["stay", "s"]];
@@ -32,14 +32,32 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-function answerIsHitOrStay(answer) {
-  return HIT_OR_STAY[0].includes(answer) || HIT_OR_STAY[1].includes(answer)
+function welcome() {
+  console.clear();
+  prompt(`Welcome to "Twenty One"!`);
+  console.log("=============================");
+  console.log("");
+  prompt(`The goal is to keep the value of your cards below 22 while having a higher score than the dealer!`);
+  console.log("");
+  prompt(`1. Both players have a starting hand of two cards.`);
+  prompt(`2. The dealer always plays with one open card!`);
+  prompt(`3. You start by comparing your cards' values to your oponents card`);
+  prompt(`4. If you think, you can beat your oponents hand, you "stay" => end your turn`);
+  prompt(`5. Otherwise you "hit" => draw a card`);
+  prompt(`6. if your cards' values surpass 21, you "bust" and lose the game. Same goes for the dealer`);
+  console.log("");
+  prompt("Press Enter to start the game!");
+  RL_SYNC.question();
 }
 
-function repeatUserInput(msg) {
+function answerIsHitOrStay(answer) {
+  return HIT_OR_STAY[0].includes(answer) || HIT_OR_STAY[1].includes(answer);
+}
+
+function repeatPlayerChoice(msg) {
   prompt(msg);
 
-  return RL_SYNC.question();
+  return RL_SYNC.question().toLowerCase();
 }
 
 function shuffle(deck) {
@@ -74,7 +92,7 @@ function totalCardsValue(cards) {
   let sum = 0;
 
   values.forEach(value => {
-    if (value === "A") {
+    if (value === ACE) {
       sum += 11;
     } else if (FACE_10.includes(value)) {
       sum += 10;
@@ -94,44 +112,76 @@ function busted(cards) {
   return totalCardsValue(cards) > 21;
 }
 
-function playerTurn(playerTurn) {
-  do {
-    prompt(("hit or stay? (h/s)"));
-    playerTurn = RL_SYNC.question().toLowerCase();
+function hand(cards) {
+  return cards.map(card => `${card[1]}${card[0]}`).join(" ");
+}
 
-    while (!answerIsHitOrStay(playerTurn)) {
-      playerTurn = repeatUserInput("no...hit or stay? Those are the only options");
-    }
+function openCard(cards) {
+  let newArrangement = cards.map(card => `${card[1]}${card[0]}`);
+  return newArrangement[0];
+}
 
-    if (HIT_OR_STAY[0].includes(playerTurn)) {
-      // insert action of drawing a card
-    }
+function addCardToHand(cards, deck) {
+  cards.push(deck.pop());
+}
 
-  } while (!HIT_OR_STAY[1].includes(playerTurn) || !busted());
+function displayHand(cards) {
+  prompt(`Your cards are now: ${hand(cards)}`);
+}
 
-  if (busted()) {
-    // end game. Play again?
-  } else {
-    prompt("You chose to stay!");
+function displayInitialHands(playerCards, dealerCards) {
+  prompt(`Dealer has ${openCard(dealerCards)} and ?`);
+  displayHand(playerCards);
+}
+
+function popTwoFromDeck(deck) {
+  return [deck.pop(), deck.pop()];
+}
+
+function getInitialHands(playerCards, dealerCards, deck) {
+  playerCards.push(...popTwoFromDeck(deck));
+  dealerCards.push(...popTwoFromDeck(deck));
+}
+
+// eslint-disable-next-line max-lines-per-function
+function playerTurn(playerCards, deck) {
+  while (true) {
+    let playerChoice;
+    do {
+      prompt(("hit or stay? (h/s)"));
+      playerChoice = RL_SYNC.question().toLowerCase();
+
+      while (!answerIsHitOrStay(playerChoice)) {
+        playerChoice = repeatPlayerChoice("no...(h)it or (s)tay? Those are the only options");
+      }
+
+      if (HIT_OR_STAY[0].includes(playerChoice)) {
+        addCardToHand(playerCards, deck);
+        prompt("You chose to hit!");
+        displayHand(playerCards);
+      }
+    } while (!HIT_OR_STAY[1].includes(playerChoice) && !busted(playerCards));
+
+    if (busted(playerCards)) break;
+
+    prompt(`You chose to stay at ${totalCardsValue(playerCards)}!`);
+    break;
   }
 }
-// work in process
-function dealerTurn(cardsValue) {
-  let answer;
-  do {
-    if (cardsValue >= 17) {
-      prompt("hit or stay? (h/s)");
-      answer = RL_SYNC.question();
-    } else {
-      answer = "hit";
-      prompt(`Dealer Value is too low to stay`);
-    }
-  } while (answer !== "stay" || busted());
 
-  if (busted()) {
-    // end game. Play again?
+function dealerTurn(dealerCards, deck) {
+  prompt(`Dealer turn...`);
+
+  while (totalCardsValue(dealerCards) < 17) {
+    prompt(`Dealer hits!`);
+    addCardToHand(dealerCards, deck);
+    prompt(`Dealer's cards are now ${hand(dealerCards)}`);
+  }
+
+  if (busted(dealerCards)) {
+    prompt(`Dealer total is now ${totalCardsValue(dealerCards)}`);
   } else {
-    prompt("Dealer chose to stay!");
+    prompt(`Dealer stays at ${totalCardsValue(dealerCards)}`);
   }
 }
 
@@ -173,21 +223,52 @@ function displayResult(playerCards, dealerCards) {
   }
 }
 
-function playRound() {
-  while (true) {
-    let deck = initializeDeck();
-    let playerCards = [];
-    let dealerCards = [];
-
-    getInitialHands(playerCards, dealerCards, deck);
-    displayInitialHands(playerCards, dealerCards);
-
-    playerTurn(playerCards, deck);
-    dealerTurn(dealerCards, deck);
-
-    getResult(playerCards, dealerCards);
-    displayResult(playerCards, dealerCards);
-
-    if (!playAgain()) break;
-  }
+function displayResultWhenBothStay(playerCards, dealerCards) {
+  console.log("<================================>");
+  prompt(`Dealer has ${dealerCards}, for a total of: ${totalCardsValue(dealerCards)}`);
+  prompt(`You have ${playerCards}, for a total of: ${totalCardsValue(playerCards)}`);
+  console.log("<================================>");
+  displayResult(playerCards, dealerCards);
 }
+
+function playRound() {
+  let deck = initializeDeck();
+  let playerCards = [];
+  let dealerCards = [];
+
+  getInitialHands(playerCards, dealerCards, deck);
+  displayInitialHands(playerCards, dealerCards);
+  debugger;
+  playerTurn(playerCards, deck);
+  if (busted(playerCards)) {
+    return displayResult(playerCards, dealerCards);
+  }
+  dealerTurn(dealerCards, deck);
+  if (busted(dealerCards)) {
+    return displayResult(playerCards, dealerCards);
+  }
+  return displayResultWhenBothStay(playerCards, dealerCards);
+}
+
+function playAgain() {
+  prompt(`Would you like to play another round? (y/n)`);
+  let answer = RL_SYNC.question().toLowerCase();
+
+  while (!YES_OR_NO.includes(answer)) {
+    prompt(`I must have blinked..what were you saying? (y/n)`);
+    answer = RL_SYNC.question().toLowerCase();
+  }
+
+  return answer[0] === "y";
+}
+
+// start game
+welcome();
+
+while (true) {
+  console.clear();
+  playRound();
+  if (!playAgain()) break;
+}
+console.clear();
+prompt("Thanks for playing Twenti One!");
