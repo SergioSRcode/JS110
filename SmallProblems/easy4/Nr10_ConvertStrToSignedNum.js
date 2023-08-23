cb4e6fa8f6f032e295acb1acbd17bb164ae1fc75
Sergio Ravera <sergioravera@hotmail.de>
2023-08-23 07:38:38 +0200

    added Convert String to Signed Number exercise

---
+++ SmallProblems/easy4/Nr10_ConvertStrToSignedNum.js
@@ -0,0 +1,32 @@
+function stringToInteger(string) {
+  const DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9
+  };
+  let arrayOfDigits = string.split("").map(char => DIGITS[char]);
+  let value = 0;
+  arrayOfDigits.forEach(digit => (value = (10 * value) + digit));
+  return value;
+}
+
+function stringToSignedInteger(str) {
+  if (str[0] === "-") {
+    return -stringToInteger(str.slice(1));
+  } else if (str[0] === "+") {
+    return stringToInteger(str.slice(1));
+  } else {
+    return stringToInteger(str);
+  }
+}
+
+console.log(stringToSignedInteger("4321") === 4321); // logs true
+console.log(stringToSignedInteger("-570") === -570); // logs true
+console.log(stringToSignedInteger("+100") === 100); // logs true
