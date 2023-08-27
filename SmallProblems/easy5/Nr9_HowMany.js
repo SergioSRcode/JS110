2dc75afc63614c5791eaf44f00f9f59ce6bd2041
Sergio Ravera <sergioravera@hotmail.de>
2023-08-27 12:00:59 +0200

    added How many? exercise

---
+++ SmallProblems/easy5/Nr9_HowMany.js
@@ -0,0 +1,24 @@
+// Write a function that counts the number of occurrences of each element in a given array.
+// Once counted, log each element alongside the number of occurrences. 
+// Consider the words case sensitive e.g. ("suv" !== "SUV").
+
+function countOccurences(vehicles) {
+  let nonDuplicates = {};
+
+  vehicles = vehicles.map(elem => elem.toLowerCase());
+
+  vehicles.forEach(vehicle => nonDuplicates[vehicle] = nonDuplicates[vehicle] + 1 || 1);
+
+  Object.keys(nonDuplicates).forEach(key => console.log(`${key} => ${nonDuplicates[key]}`));
+}
+
+let vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
+                'motorcycle', 'motorcycle', 'car', 'truck', 'suv'];
+
+countOccurences(vehicles);
+
+// console output -- your output sequence may be different
+car => 4
+truck => 3
+suv => 2
+motorcycle => 2
