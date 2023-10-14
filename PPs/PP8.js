c933bb7ea3262d5ad163143df57abb95ed77510a
Sergio Ravera <sergioravera@hotmail.de>
2023-10-14 12:41:37 +0200

    added Practice Problem 8

---
+++ PPs/PP8.js
@@ -0,0 +1,16 @@
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "Bambam"];
+
+// Write a program that uses this array to create an object where the names are the keys and 
+// the values are the positions in the array:
+
+// { Fred: 0, Barney: 1, Wilma: 2, Betty: 3, Pebbles: 4, Bambam: 5 }
+
+function switchUp(arr) {
+  let obj = {};
+
+  arr.forEach((word, idx) => obj[word] = idx);
+
+  return obj;
+}
+
+console.log(switchUp(flintstones));
