6920822042a9a9aeade742d8833df05e9f84cbbc
Sergio Ravera <sergioravera@hotmail.de>
2023-10-14 12:46:29 +0200

    added Practice Problem 9

---
+++ PPs/PP9.js
@@ -0,0 +1,14 @@
+// Add up all of the ages from the Munster family object:
+
+let ages = {
+  Herman: 32,
+  Lily: 30,
+  Grandpa: 5843,
+  Eddie: 10,
+  Marilyn: 22,
+  Spot: 237
+};
+
+let sum = Object.values(ages).reduce((age, acc) => age + acc, 0);
+
+console.log(sum);
